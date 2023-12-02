0df9176f2fa4d84586aac94e31ff9be8b5885a12
AnuJ <70368478+eriven@users.noreply.github.com>
2023-12-03 00:38:33 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,13 @@
+
+    # Using a while loop to print numbers from 1 to 10
+    num = 1
+    while num <= 10:
+        print(num)
+        num += 1
+
+    # Creating a list of names
+    names = ["Alice", "Bob", "Charlie", "David"]
+
+    # Using a for loop to print personalized greetings for each name
+    for name in names:
+        print("Hello, " + name + "! How are you?")
