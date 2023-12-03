7899a925fc9703a5f4a4fff6dd2a1f5f04e05897
AnuJ <70368478+eriven@users.noreply.github.com>
2023-12-03 19:18:40 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,13 @@
+
+#Define a Python function named is_even that takes an integer parameter 
+#and returns True if the provided number is even;
+#otherwise, it returns False.
+def is_even(n):
+ if n%2==0:
+  return True
+ else:
+  return False
+
+result1=is_even(8)
+result2=is_even(9)
+print(result1,result2)
