87817ad10b66ff22666d2e5f3e323ddccbafc1cc
AnuJ <70368478+eriven@users.noreply.github.com>
2023-12-06 05:33:58 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,1 +1,13 @@
+def define_age_group(age):
+    if age < 18:
+        return "You are a minor."
+    elif age <= 65:
+        return "You are an adult."
+    else:
+        return "You are a senior citizen."
+
+# Example usage
+age_input = int(input("Enter your age: "))
+message = define_age_group(age_input)
+print(message)
 
